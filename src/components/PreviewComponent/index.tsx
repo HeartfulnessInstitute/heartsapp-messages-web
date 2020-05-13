c4047e0d4472fc182ee5aca436a782c2d8e205e5
Vani Yaji <vaniyaji@Vanis-MacBook-Pro.local>
2020-05-13 11:19:08 +0530

    youtube url fix

--- src/components/PreviewComponent/index.tsx
+++ src/components/PreviewComponent/index.tsx
@@ -3,15 +3,15 @@ import { Card } from 'antd';
 
 const PreviewComponent = (props) => {
    const modalTitle=props.formData.title
-//    const modalUrl=props.formData.url.split('/')
-    
+   const modalUrl=props.formData.url.split('/')
+    console.log(props.formData.url)
     return (
         <Card>
         <h2>
             {modalTitle}
         </h2>
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARwAAACxCAMAAAAh3/JWAAAAA1BMVEXy8vJkA4prAAAASElEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIALA8UNAAFusnLHAAAAAElFTkSuQmCC" />
-        {/* <iframe width="420" height="345" src={"https://www.youtube.com/embed/"+modalUrl[modalUrl.length-1]} allowFullScreen/> */}
+        {/* <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARwAAACxCAMAAAAh3/JWAAAAA1BMVEXy8vJkA4prAAAASElEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIALA8UNAAFusnLHAAAAAElFTkSuQmCC" /> */}
+        <iframe width="420" height="345" src={"https://www.youtube.com/embed/"+modalUrl[modalUrl.length-1]} allowFullScreen/>
     </Card>
     )
 }
